6910a0ea1c48e11c0bbc8ad263cdd2d895aba98b
baseba <sebastin.espejo@uc.cl>
2021-05-28 14:57:59 -0400

    cs v4

--- Sites/consultas/vehiculos_de_comuna.php
+++ Sites/consultas/vehiculos_de_comuna.php
@@ -10,7 +10,7 @@
  	$query = "SELECT vehiculos FROM unidades, vehiculos, direcciones
    WHERE unidades.id = direcciones.id
    AND vehiculos.unidad = unidades.id
-   AND lower(direcciones.comuna) = $comuna_elegida  -- aqui se pone la comuna con lo que dice el pdf
+   AND direcciones.comuna = lower($comuna_elegida)  -- aqui se pone la comuna con lo que dice el pdf
    ;";
 	$result = $db -> prepare($query);
 	$result -> execute();
